5348c424b2a6446b4501c32410a8af4765fa14cc
AkbalAbuHanifa32 <abu120041@gmail.com>
2022-04-19 06:29:08 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#991b1b',
+        dark: '#1c1917',
+        caca: '#200122',
+        akbal: '#6f0000'
+      }
+    },
+  },
+  plugins: [],
+}
